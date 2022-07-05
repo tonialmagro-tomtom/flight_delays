2c2c14ff575be2e4e991e834026e8faa90745f34
Toni <toni@portentio.com>
2022-07-05 17:15:14 +0200

    Revert "Revert "first commit""
    This reverts commit a402b3c290651bab49e3a058903789ee911c1619.

---
+++ src/flight_delay/pipeline.py
@@ -0,0 +1,55 @@
+"""
+This is a boilerplate pipeline
+generated using Kedro 0.18.1
+"""
+
+from kedro.pipeline import Pipeline, node, pipeline
+
+from .nodes import (
+    select_cols,
+    feature_engineering,
+    make_pyspark_pipeline,
+    make_predictions,
+    report_accuracy,
+    split_data,
+)
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline(
+        [
+            node(
+                func=select_cols,
+                inputs=["flights", "parameters"],
+                outputs=[
+                    "flights_reduced",
+                ],
+                name="select_cols",
+            ),
+            node(
+                func=feature_engineering,
+                inputs=["flights_reduced", "parameters"],
+                outputs=[
+                    "flights_extra",
+                ],
+                name="feature_engineering",
+            ),
+            node(
+                func=split_data,
+                inputs=["flights_extra", "parameters"],
+                outputs=[
+                    "data_train",
+                    "data_test",
+                ],
+                name="split",
+            ),
+            node(
+                func=make_pyspark_pipeline,
+                inputs=["data_train", "parameters"],
+                outputs=[
+                    "pipeline_classifier",
+                ],
+                name="fit_pipeline",
+            ),
+        ]
+    )
